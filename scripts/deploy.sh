d0482d98ee0099e8783bf4f7535a3fad0095c4ed
phouse512 <philiphouse2015@u.northwestern.edu>
2023-12-30 15:37:17 -0600

    les go

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -12,6 +12,6 @@ pip install awscli --upgrade --user
 ls 
 ls _site
 echo "Beginning deploy"
-~/.local/bin/aws s3 sync ./_site s3://phizzle.space
+~/.local/bin/aws s3 sync ./_build s3://phizzle.space
 ~/.local/bin/aws cloudfront create-invalidation --distribution-id E1HVGFDORPL5AZ --paths /\*
 
